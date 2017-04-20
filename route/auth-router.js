bd82fae1bfb27ed4f3316a322f3d3347b6b44a39
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-04-20 16:03:44 -0700

    generating token backend and front end working

--- route/auth-router.js
+++ route/auth-router.js
@@ -1,31 +1,28 @@
 'use strict';
 
+const http = require('http');
+const OAuth = require('oauth').OAuth;
 const Router = require('express').Router;
 const createError = require('http-errors');
 const jsonParser = require('body-parser').json();
-var http = require('http')
-var OAuth = require('oauth').OAuth
-var url = require('url');
-var session = require('express-session');
-const trelloOAUTH = require('../lib/trello-oauth-middleware.js');
+const session = require('express-session');
 const debug = require('debug')('abba:auth-router');
+
+const trelloOAUTH = require('../lib/trello-oauth-middleware.js');
 
 const authRouter = module.exports = Router();
 
-var access_token;
-
 authRouter.get('/api/auth/login', trelloOAUTH.generateToken, function(req, res, next) {
   console.log('I am in login route');
-  access_token = req.trelloOAUTH.token_secret;
   res.send(JSON.stringify({ oauth_token: req.trelloOAUTH.token, name: req.trelloOAUTH.app_name }));
   next();
 })
 
 authRouter.get('/api/auth/trello/callback',trelloOAUTH.generateAccessToken, function(req, res, next) {
   debug('GET /api/auth/trello/callback');
-    console.log('req.session in callback-->',req.session)
-  //  return res.redirect(`home`)
-    next();
+
+    console.log('I in callback-->in callback route', req.accessToken)
+    return res.redirect(`/#/home?token=${req.query.oauth_token}`);
 });
 
 authRouter.get('/api/trello/boards', trelloOAUTH.userAccess, function(req, res, next) {
@@ -63,6 +60,3 @@ authRouter.get('/api/trello/boards/:id/cards', trelloOAUTH.userAccess, function(
      }
    );
 });
-
-//http://moonlitscript.com/post.cfm/how-to-use-oauth-and-twitter-in-your-node-js-expressjs-app/
-// In a real app, the accessToken and accessTokenSecret should be stored
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Location } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'landing',
@@ -10,13 +10,21 @@ import { Router } from '@angular/router';
 })
 
 export class LandingComponent {
-  constructor(private http: Http, private _router: Router, private _location: Location) {}
+  constructor(private http: Http, private _router: Router, private _route: ActivatedRoute, private _location: Location) {}
   login() {
     console.log('in logi method')
     return this.http.get(`http://localhost:4000/api/auth/login`)
     .subscribe((res: Response) => {
        let data = res.json();
        window.location.href = `https://trello.com/1/OAuthAuthorizeToken?oauth_token=${data.oauth_token}&name=${data.name}`;
+    })
+  }
+
+  redirect_home() {
+    this._route.queryParams.subscribe(params => {
+      console.log('in the redirect', params);
+      if (params['oauth_verifier'])
+         this._router.navigate(['home']);
     })
   }
 }
